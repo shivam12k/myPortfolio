2b5c121d61d56141e9c115d152373071223c2c59
Shivam <mynameisshivam78@gmail.com>
2024-03-25 21:30:04 +0530

    change the design of project section

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,39 +1,25 @@
-import "./card.css";
-const Card = ({data}) => {
+import React from 'react'; // Don't forget to import React when using JSX
+import './card.css';
+
+const Card = ({ data }) => {
+  console.log(data);
   return (
-    <div className="card ">
+    <div className="card">
       <div className="card2">
         <div className="relative mx-4 -mt-2 h-40 overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40 bg-gradient-to-r from-violet-500 to-violet-600">
           <img src="" alt="" />
         </div>
         <div className="p-6">
-
           <h5 className="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
-           {data.Name}
+            {data.Name}
           </h5>
           <p className="block font-sans text-base font-light leading-relaxed text-inherit antialiased">
-           {data.discription}
+            {data.description}
           </p>
-          {/* purchace Button */}
-         <div className="w-full  flex flex-row-reverse ">
-         <div data-tooltip={`Price: ${data.price}`} className="button">
-            <div className="button-wrapper">
-              <div className="text">Buy Now</div>
-              <span className="icon">
-                <svg
-                  viewBox="0 0 16 16"
-                  className="bi bi-cart2"
-                  fill="currentColor"
-                  height="16"
-                  width="16"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"></path>
-                </svg>
-              </span>
-            </div>
+          {/* purchase Button */}
+          <div className="w-full flex flex-row-reverse">
+          <a type="link" href='${data.GitHub}'  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">view</a>
           </div>
-         </div>
         </div>
       </div>
     </div>
